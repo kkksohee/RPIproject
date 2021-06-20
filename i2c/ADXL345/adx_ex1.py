d5317ea5045aec4d56d220ee1daa8650f5965da0
kkksohee <83942772+kkksohee@users.noreply.github.com>
2021-06-20 23:38:04 +0900

    Update adx_ex1.py

--- i2c/ADXL345/adx_ex1.py
+++ i2c/ADXL345/adx_ex1.py
@@ -6,4 +6,53 @@ bus=smbus.SMbus(1)
 
 address=0x53
 
-x_adr
+# x-axis, y-axis, z-axis adress
+x_adr = 0x32
+y_adr = 0x34
+z_adr = 0x36
+
+# ADXL345 init
+def init_ADXL345():    
+    bus.write_byte_data(address, 0x2D, 0x08)
+
+# data measure
+def measure_acc(adr):    
+    acc0 = bus.read_byte_data(address, adr)
+
+    acc1 = bus.read_byte_data(address, adr + 1)
+
+    acc = (acc1 << 8) + acc0
+
+    if acc > 0x1FF:
+        acc = (65536 - acc) * -1
+
+    acc = acc * 3.9 / 1000
+
+    return acc
+
+  def main():
+    print(bus)
+    init_ADXL345()
+    mylcd=LCD.lcd()
+
+    a=input()
+    if a=='1':
+        mylcd.lcd_display_string("PRi I2C test",1)
+        sleep(2)
+        mylcd.lcd_clear()
+
+    elif a=='2':
+        x_acc = measure_acc(x_adr)
+        y_acc = measure_acc(y_adr)
+        z_acc = measure_acc(z_adr)
+
+        print ('X = %2.2f' % x_acc, '[g], Y = %2.2f' % y_acc, '[g], Z = %2.2f' % z_acc, '[g]')
+
+        sleep(1)
+        
+    else:
+        print('error')
+    
+        
+if __name__ == '__main__':
+    main()
