17d9d3a335050a3b54221a091e8617e7ee8a5e9e
kkksohee <83942772+kkksohee@users.noreply.github.com>
2021-06-21 01:23:51 +0900

    Update dot_color.py

--- spi/dot_color.py
+++ spi/dot_color.py
@@ -3,13 +3,21 @@
 # Copyright (c) 2017-18 Richard Hull and contributors
 # See LICENSE.rst for details.
 
+import re
 import time
+import argparse
 
 from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
-device = max7219(width=8, height=1)
 
+ serial = spi(port=0, device=0, gpio=noop())
+ device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation,
+                     rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
 
 def rotate(l):
     return l[-1:] + l[:-1]
