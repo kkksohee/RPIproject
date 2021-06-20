0c80aa40d30085fdb2bbfa5d0c2c2f335f645abf
kkksohee <83942772+kkksohee@users.noreply.github.com>
2021-06-21 01:26:22 +0900

    Update dot_color.py

--- spi/dot_color.py
+++ spi/dot_color.py
@@ -23,8 +23,11 @@ def rotate(l):
     return l[-1:] + l[:-1]
 
 
-def main():
-    colors = [
+def demo(n, block_orientation, rotate, inreverse):
+ serial = spi(port=0, device=0, gpio=noop())
+ device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation,
+                     rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
+ colors = [
         "red",
         "orange",
         "yellow",
@@ -35,30 +38,40 @@ def main():
         "white"
     ]
 
-    for color in colors:
-        with canvas(device) as draw:
-            draw.line(device.bounding_box, fill=color)
-        time.sleep(2)
+  for color in colors:
+      with canvas(device) as draw:
+          draw.line(device.bounding_box, fill=color)
+      time.sleep(2)
 
-    device.contrast(0x30)
-    for _ in range(80):
-        with canvas(device) as draw:
-            for x, color in enumerate(colors):
-                draw.point((x, 0), fill=color)
+  device.contrast(0x30)
+  for _ in range(80):
+      with canvas(device) as draw:
+          for x, color in enumerate(colors):
+              draw.point((x, 0), fill=color)
 
-        colors = rotate(colors)
-        time.sleep(0.2)
+      colors = rotate(colors)
+      time.sleep(0.2)
 
-    time.sleep(4)
+  time.sleep(4)
 
-    device.contrast(0x80)
-    time.sleep(1)
-    device.contrast(0x10)
-    time.sleep(1)
+  device.contrast(0x80)
+  time.sleep(1)
+  device.contrast(0x10)
+  time.sleep(1)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='matrix_demo arguments',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--cascaded', '-n', type=int, default=1, help='Number of cascaded MAX7219 LED matrices')
+    parser.add_argument('--block-orientation', type=int, default=0, choices=[0, 90, -90], help='Corrects block orientation when wired vertically')
+    parser.add_argument('--rotate', type=int, default=0, choices=[0, 1, 2, 3], help='Rotate display 0=0°, 1=90°, 2=180°, 3=270°')
+    parser.add_argument('--reverse-order', type=bool, default=False, help='Set to true if blocks are in reverse order')
+
+    args = parser.parse_args()
+
     try:
-        main()
+        demo(args.cascaded, args.block_orientation, args.rotate, args.reverse_order)
     except KeyboardInterrupt:
         pass
